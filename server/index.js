7c0e0187d3a5e61c7625b4be15ec5d336156b191
dhansenaz <dhansen@allthingsdot.com>
2018-04-17 12:53:12 -0700

    homepage_layout

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ctrl = require('./controller.js');
+const PORT = 4000;
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log("You are running on port 4000"))
--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import './App.css';
 import Nav from './components/Nav'
 import GetStarted from './components/GetStarted'
 import ImageHome from './components/ImageHome'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div>
             <Nav />
               <div className="center">
                 <ImageHome />
-                  <div>
-                    <p>while everyone else is searching....you could be submitting..</p>
-                  </div>
+                    <div className="p-container">
+                      <p>while everyone else is searching....you could be submitting...</p>
+                    </div>
                 <GetStarted />
+                <Footer />
+                  
               </div>
-          </div>
+              
       </div>
     );
   }
